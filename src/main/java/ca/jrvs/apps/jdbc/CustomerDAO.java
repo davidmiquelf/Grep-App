1cf2504f8057c69c281014c5fdc0aba4111c18bc
David Faig <david.faig.jrvs.ca@gmail.com>
2019-06-28 17:00:47 +0000

    Implemented Update

--- src/main/java/ca/jrvs/apps/jdbc/CustomerDAO.java
+++ src/main/java/ca/jrvs/apps/jdbc/CustomerDAO.java
@@ -11,6 +11,10 @@ public class CustomerDAO extends DataAccessObject<Customer> {
     private static final String INSERT = "INSERT INTO customer (first_name, last_name," +
             "email, phone, address, city, state, zipcode) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
 
+    private static final String UPDATE = "UPDATE customer SET (first_name, last_name," +
+            "email, phone, address, city, state, zipcode) = (?, ?, ?, ?, ?, ?, ?, ?)" +
+            "WHERE id = '?'";
+
     public CustomerDAO(Connection connection) {
         super(connection);
     }
@@ -27,12 +31,26 @@ public class CustomerDAO extends DataAccessObject<Customer> {
 
     @Override
     public Customer update(Customer dto) {
-        return null;
+        try(PreparedStatement statement = this.connection.prepareStatement(UPDATE)){
+            statement.setString(1, dto.getFirstName());
+            statement.setString(2, dto.getLastName());
+            statement.setString(3, dto.getEmail());
+            statement.setString(4, dto.getPhone());
+            statement.setString(5, dto.getAddress());
+            statement.setString(6, dto.getCity());
+            statement.setString(7, dto.getState());
+            statement.setString(8, dto.getZipCode());
+            statement.setString(9, Long.toString(dto.getId()));
+            statement.execute();
+            return null;
+        }catch(SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
     }
-
     @Override
     public Customer create(Customer dto) {
-        try(PreparedStatement statement = this.connection.prepareStatement(INSERT);){
+        try(PreparedStatement statement = this.connection.prepareStatement(INSERT)){
             statement.setString(1, dto.getFirstName());
             statement.setString(2, dto.getLastName());
             statement.setString(3, dto.getEmail());
