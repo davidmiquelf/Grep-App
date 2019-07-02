ea59615f7c006fc9e064358493501acddfb77571
David Faig <david.faig.jrvs.ca@gmail.com>
2019-07-02 15:56:14 +0000

    fixed bugs

--- src/main/java/ca/jrvs/apps/jdbc/CustomerDAO.java
+++ src/main/java/ca/jrvs/apps/jdbc/CustomerDAO.java
@@ -11,15 +11,13 @@ import java.util.List;
 
 public class CustomerDAO extends DataAccessObject<Customer> {
     private static final String INSERT = "INSERT INTO customer (first_name, last_name," +
-            "email, phone, address, city, state, zipcode) VALUES (?, ?, ?, ?, ?, ?, ?, ?)" +
-            "ON CONFLICT (first_name, last_name) DO UPDATE SET (first_name, last_name," +
-            "email, phone, address, city, state, zipcode) = (?, ?, ?, ?, ?, ?, ?, ?)";
+            "email, phone, address, city, state, zipcode) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
 
     private static final String UPDATE = "UPDATE customer SET (first_name, last_name," +
             "email, phone, address, city, state, zipcode) = (?, ?, ?, ?, ?, ?, ?, ?)" +
-            "WHERE id = '?'";
+            "WHERE customer_id = '?'";
 
-    private static final String SELECT_BY_ID = "SELECT * FROM customer where id = ?";
+    private static final String SELECT_BY_ID = "SELECT * FROM customer where customer_id = ?";
     private static final String SELECT_ALL = "SELECT * FROM customer";
 
     public CustomerDAO(Connection connection) {
@@ -59,7 +57,7 @@ public class CustomerDAO extends DataAccessObject<Customer> {
 
             while (rs.next()) {
                 Customer c = new Customer();
-                c.setId(rs.getLong("id"));
+                c.setId(rs.getLong("customer_id"));
                 c.setFirstName(rs.getString("first_name"));
                 c.setLastName(rs.getString("last_name"));
                 c.setEmail(rs.getString("email"));
@@ -89,12 +87,12 @@ public class CustomerDAO extends DataAccessObject<Customer> {
             statement.setString(7, dto.getState());
             statement.setString(8, dto.getZipCode());
             statement.setString(9, Long.toString(dto.getId()));
-            statement.execute();
-            return null;
+            statement.executeUpdate();
         }catch(SQLException e) {
             e.printStackTrace();
             throw new RuntimeException(e);
         }
+        return findById(dto.getId());
     }
     @Override
     public Customer create(Customer dto) {
@@ -107,18 +105,10 @@ public class CustomerDAO extends DataAccessObject<Customer> {
             statement.setString(6, dto.getCity());
             statement.setString(7, dto.getState());
             statement.setString(8, dto.getZipCode());
-            statement.setString(9, dto.getFirstName());
-            statement.setString(10, dto.getLastName());
-            statement.setString(11, dto.getEmail());
-            statement.setString(12, dto.getPhone());
-            statement.setString(13, dto.getAddress());
-            statement.setString(14, dto.getCity());
-            statement.setString(15, dto.getState());
-            statement.setString(16, dto.getZipCode());
-            statement.execute();
+            statement.executeUpdate();
             try (ResultSet genKeys = statement.getGeneratedKeys()) {
                 if (genKeys.next()) {
-                    dto.setId(genKeys.getLong("id"));
+                    dto.setId(genKeys.getLong("customer_id"));
                 } else {
                     throw new SQLException("Creating user failed, no ID obtained.");
                 }
--- src/main/java/ca/jrvs/apps/jdbc/Main.java
+++ src/main/java/ca/jrvs/apps/jdbc/Main.java
@@ -5,22 +5,13 @@ import java.sql.DriverManager;
 
 public class Main {
     public static void main(String[] Args) {
-        Customer c = new Customer();
-        c.setFirstName("David");
-        c.setLastName("Faig");
-        c.setAddress("here");
-        c.setEmail("david.faig.jrvs.ca@gmail.com");
-        c.setCity("Toronto");
-        c.setZipCode("ziphere");
-        c.setState("Ontario");
-        c.setPhone("1231231234");
 
         try (Connection conn = DriverManager.getConnection
                 (
                         "jdbc:postgresql://localhost:5432/hplussport", "postgres", "password"
                 )) {
             CustomerDAO dao = new CustomerDAO(conn);
-            Long id = dao.create(c).getId();
+            Long id = Long.valueOf(10000);
 
             Customer c_db = dao.findById(id);
             System.out.println(c_db.toString());
