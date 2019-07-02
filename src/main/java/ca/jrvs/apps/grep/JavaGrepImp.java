dda104ee808bade2bf01f38afabe1ae004f7c551
David Faig <david.faig.jrvs.ca@gmail.com>
2019-07-02 20:04:18 +0000

    Cleaned up code more.

--- src/main/java/ca/jrvs/apps/grep/JavaGrepImp.java
+++ src/main/java/ca/jrvs/apps/grep/JavaGrepImp.java
@@ -1,142 +1,147 @@
 package ca.jrvs.apps.grep;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class JavaGrepImp implements JavaGrep {
-    private String regex;
-    private String outFile;
-    private String rootPath;
 
-    public JavaGrepImp(String regex, String rootPath, String outFile){
-        this.regex = regex;
-        this.outFile = outFile;
-        this.rootPath = rootPath;
+  private String regex;
+  private String outFile;
+  private String rootPath;
 
+  public JavaGrepImp(String regex, String rootPath, String outFile) {
+    this.regex = regex;
+    this.outFile = outFile;
+    this.rootPath = rootPath;
+
+  }
+
+  public static void main(String[] args) {
+
+    if (args.length != 3) {
+      throw new IllegalArgumentException("USAGE: regex rootPath outFile");
     }
 
+    JavaGrepImp javaGrepImp = new JavaGrepImp(args[0], args[1], args[2]);
 
-    @Override
-    public void process() throws IOException {
+    try {
+      javaGrepImp.process();
 
-        List<File> files = listFiles(rootPath);
-        ArrayList<String> outputLines = new ArrayList<>();
-        List<String> fileLines;
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 
-        for (File file : files){
-            fileLines = readLines(file);
+  @Override
+  public void process() throws IOException {
 
-            for (String line : fileLines){
-                if(containsPattern(line)){
-                    outputLines.add(line);
-                }
-            }
+    List<File> files = listFiles(rootPath);
+    ArrayList<String> outputLines = new ArrayList<>();
+    List<String> fileLines;
+
+    for (File file : files) {
+      fileLines = readLines(file);
+
+      for (String line : fileLines) {
+        if (containsPattern(line)) {
+          outputLines.add(line);
         }
-        writeToFile(outputLines);
+      }
+    }
+    writeToFile(outputLines);
+  }
+
+  @Override
+  public List<File> listFiles(String rootDir) {
+    File rootFolder = new File(rootDir);
+    List<File> fileList = new ArrayList<>();
+    File[] files = rootFolder.listFiles();
+
+    if (files == null) {
+      fileList.add(rootFolder);
+      return fileList;
     }
 
-    @Override
-    public List<File> listFiles(String rootDir) {
-        File rootFolder = new File(rootDir);
-        List<File> fileList = new ArrayList<>();
-        File [] files = rootFolder.listFiles();
+    for (File fileEntry : files) {
+      if (fileEntry.isDirectory()) {
+        fileList.addAll(listFiles(fileEntry.getAbsolutePath()));
+      } else {
+        fileList.add(fileEntry);
+      }
+    }
+    return fileList;
+  }
 
-        if (files == null){
-            fileList.add(rootFolder);
-            return fileList;
-        }
+  @Override
+  public List<String> readLines(File inputFile) throws IOException {
+    ArrayList<String> lines = new ArrayList<>();
 
-        for (File fileEntry : files) {
-            if (fileEntry.isDirectory()) {
-                fileList.addAll(listFiles(fileEntry.getAbsolutePath()));
-            } else {
-                fileList.add(fileEntry);
-            }
-        }
-        return fileList;
+    BufferedReader br = new BufferedReader(new FileReader(inputFile));
+    String line;
+
+    while ((line = br.readLine()) != null) {
+      lines.add(line);
+    }
+    br.close();
+    return lines;
+
+  }
+
+  @Override
+  public Boolean containsPattern(String line) {
+    return line.matches(this.regex);
+  }
+
+  @Override
+  public void writeToFile(List<String> lines) throws IOException {
+
+    //Check if there is an outfile.
+    if (this.outFile == null) {
+      return;
     }
 
-    @Override
-    public List<String> readLines(File inputFile) throws IOException{
-        ArrayList<String> lines = new ArrayList<>();
+    BufferedWriter writer = new BufferedWriter(new FileWriter(this.outFile));
 
-        BufferedReader br = new BufferedReader(new FileReader(inputFile));
-        String line;
-
-        while ((line = br.readLine()) != null) {
-            lines.add(line);
-        }
-        br.close();
-        return lines;
-
+    for (String line : lines) {
+      writer.write(line);
     }
+    writer.close();
 
-    @Override
-    public Boolean containsPattern(String line) {
-        return line.matches(this.regex);
-    }
+  }
 
-    @Override
-    public void writeToFile(List<String> lines) throws IOException {
+  @Override
+  public String getRootPath() {
+    return this.rootPath;
+  }
 
-        //Check if there is an outfile.
-        if (this.outFile == null){
-            return;
-        }
+  @Override
+  public void setRootPath(String rootPath) {
+    this.rootPath = rootPath;
+  }
 
-        BufferedWriter writer = new BufferedWriter (new FileWriter(this.outFile));
+  @Override
+  public String getRegex() {
+    return this.regex;
+  }
 
-        for (String line : lines) {
-            writer.write(line);
-        }
-        writer.close();
+  @Override
+  public void setRegex(String regex) {
+    this.regex = regex;
+  }
 
-    }
+  @Override
+  public String getOutFile() {
+    return this.outFile;
+  }
 
-    @Override
-    public String getRootPath() {
-        return this.rootPath;
-    }
-
-    @Override
-    public void setRootPath(String rootPath) {
-        this.rootPath = rootPath;
-    }
-
-    @Override
-    public String getRegex() {
-        return this.regex;
-    }
-
-    @Override
-    public void setRegex(String regex) {
-        this.regex = regex;
-    }
-
-    @Override
-    public String getOutFile() {
-        return this.outFile;
-    }
-
-    @Override
-    public void setOutFile(String outFile) {
-        this.outFile = outFile;
-    }
-
-    public static void main(String[] args){
-
-        if (args.length != 3) {
-            throw new IllegalArgumentException("USAGE: regex rootPath outFile");
-        }
-
-        JavaGrepImp javaGrepImp = new JavaGrepImp(args[0], args[1], args[2]);
-
-        try {
-            javaGrepImp.process();
-
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-    }
+  @Override
+  public void setOutFile(String outFile) {
+    this.outFile = outFile;
+  }
 }
