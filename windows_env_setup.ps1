ed72a673619d75b0860550531370e2c5f0b52e53
Davidmiquelf <david.faig.jrvs.ca@gmail.com>
2019-07-15 14:06:58 -0400

    edit to setup script

--- windows_env_setup.ps1
+++ windows_env_setup.ps1
@@ -3,3 +3,4 @@ Write-Output '$ENV:accessToken += "<YOUR ACCESS SECRET>" ' | Out-File $Profile -
 Write-Output '$ENV:consumerSecret += "<YOUR CONSUMER SECRET>"' | Out-File $Profile -append -encoding ascii
 Write-Output '$ENV:consumerKey += "<YOUR CONSUMER KEY>"' | Out-File $Profile -append -encoding ascii
 Write-Output '$ENV:spring_profiles_active += "dev"' | Out-File $Profile -append -encoding ascii
+& $profile
